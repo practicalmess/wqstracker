0ce33a6a49f1eb53865734848f688950af29ea19
SJ Hackney <snackney@gmail.com>
2016-01-25 22:53:29 -0500

    Added medication migration

---
+++ database/migrations/2016_01_26_032929_create_medications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('med_name');
+            $table->integer('interval');
+            $table->integer('doses_taken');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('books');
+    }
+}
