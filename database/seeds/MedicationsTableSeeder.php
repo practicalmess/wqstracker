db2c17f929361de911dd7acfd5e6320e9ddf0226
SJ Hackney <snackney@gmail.com>
2016-01-29 23:24:22 -0500

    Added all relevant medications

--- database/seeds/MedicationsTableSeeder.php
+++ database/seeds/MedicationsTableSeeder.php
@@ -16,7 +16,7 @@ class MedicationsTableSeeder extends Seeder
         	'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
         	'med_name' => 'ibuprofen',
         	'interval' => 8,
-        	'last_taken' => Carbon\Carbon::now()->subHours(9),
+        	'last_taken' => Carbon\Carbon::now(),
             ]);
         DB::table('medications')->insert([
         	'created_at' => Carbon\Carbon::now()->toDateTimeString(),
@@ -30,7 +30,21 @@ class MedicationsTableSeeder extends Seeder
             'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
             'med_name' => 'ondansetron',
             'interval' => 8,
-            'last_taken' => Carbon\Carbon::now()->subHours(8),
+            'last_taken' => Carbon\Carbon::now()->subHours(2),
+            ]);
+        DB::table('medications')->insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'med_name' => 'omeprazole',
+            'interval' => 24,
+            'last_taken' => Carbon\Carbon::now()->subHours(12),
+            ]);
+        DB::table('medications')->insert([
+            'created_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => Carbon\Carbon::now()->toDateTimeString(),
+            'med_name' => 'naproxen',
+            'interval' => 12,
+            'last_taken' => Carbon\Carbon::now()->subHours(1),
             ]);
 
     }
