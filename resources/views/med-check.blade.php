87ab860c9ce37a73a44c7b52b60e8e13d4323615
SJ Hackney <snackney@gmail.com>
2016-01-30 16:43:40 -0500

    Added button styling and cute photos

--- resources/views/med-check.blade.php
+++ resources/views/med-check.blade.php
@@ -6,17 +6,27 @@
 	@foreach($meds as $med)
 		<p>
 		{{$med->med_name}}<br>
+		@if($med->id === 3)
+			<img class="med" src="/images/ondansetron.jpg" alt="ondansetron pill"><br>
+		@elseif($med->id === 4)
+			<img class="med" src="/images/omeprazole.jpg" alt="omeprazole capsul"><br>
+		@elseif($med->id === 5)
+			<img class="med" src="/images/naproxen.jpg" alt="naproxen pill"><br>
+		@endif
+
 		@if($med->can_take === 1)
 			You can take this!
 			<form action="/med-taken" method="POST">
 				<input type="hidden" value="{{csrf_token()}}" name="_token">
 				<input type="hidden" name="id" value="{{$med->id}}">
-				<input type="submit" value="Take!">
+				<input type="submit" class="btn" value="Take!">
 			</form>
 		@else
-			You can take this at {{$med->next}}.
+			You can take this at <strong>{{$med->next}}</strong>.
 		@endif
 		</p>
 	@endforeach
+	
+	<img class="footer" src="/images/puppy1.jpg" alt="A corgi puppy">
 
 @stop
--- resources/views/med-taken.blade.php
+++ resources/views/med-taken.blade.php
@@ -2,5 +2,8 @@
 
 @section('content')
 	{{$med->med_name}} taken!
+	<br>
 	<a href="/check">Back</a>
+	<br>
+	<img class="reward" src="/images/elephants1.jpg" alt="Elephant and two baby elephants">
 @stop
