9289946e9c165577b95798fa43be4ee6975e1dee
SJ Hackney <snackney@gmail.com>
2016-01-29 23:35:25 -0500

    Viewport addition

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
 		@yield('title', 'Wobbly Queoray Tracker')
 	</title>
 	<meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css" rel="stylesheet">
     <link href="/css/main.css" type='text/css' rel='stylesheet'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
